3f2c02530f7d49c1a1d4314ea649658f5c246cb3
seong_hyun <getplay0131>
2024-11-01 17:56:31 +0900

    24.11.1 upload

---
+++ js/style-switcher.js
@@ -0,0 +1,26 @@
+const styleSwitcherToggler = document.querySelector(".style-switcher-toggler");
+styleSwitcherToggler.addEventListener("click", () => {
+  document.querySelector(".style-switcher").classList.toggle("open");
+});
+
+// scroll event close the color tab
+window.addEventListener("scroll", () => {
+  if (document.querySelector(".style-switcher").classList.contains("open")) {
+    document.querySelector(".style-switcher").classList.remove("open");
+  }
+});
+
+// =================== color change =======
+const alternateStyle = document.querySelectorAll(".alternate-style");
+function setActiveStyleChange(color) {
+  alternateStyle.forEach((style) => {
+    if (color === style.getAttribute("title")) {
+      style.removeAttribute("disabled");
+    } else {
+      style.setAttribute("disabled", "true");
+    }
+  });
+}
+
+//===== light and dark theme change =========
+const dayNight = document.querySelector(".day-night");
