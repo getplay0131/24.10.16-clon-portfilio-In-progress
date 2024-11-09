340fb5472490b65c50b63de26ddf160fc29941aa
seong_hyun <getplay0131>
2024-11-09 23:04:35 +0900

    24.11.09 update

--- js/script.js
+++ js/script.js
@@ -41,10 +41,16 @@ const nav = document.querySelector(".nav"),
   totalNavList = navList.length,
   allSection = document.querySelectorAll(".section"),
   totalSection = allSection.length;
-for (let i = 0; i < totalNavList; i++) {
+for (let i = 0; i < totalSection; i++) {
   const a = navList[i].querySelector("a");
   a.addEventListener("click", function () {
+    for (let i = 0; i < totalSection; i++) {
+      allSection[i].classList.remove("back-section");
+    }
     for (let j = 0; j < totalNavList; j++) {
+      if (navList[j].querySelector("a").classList.contains("active")) {
+        allSection[j].classList.add("back-section");
+      }
       navList[j].querySelector("a").classList.remove("active");
     }
     this.classList.add("active");
@@ -58,6 +64,20 @@ function showSection(element) {
   }
   const target = element.getAttribute("href").split("#")[1];
   document.querySelector("#" + target).classList.add("active");
+}
+
+const navTogglerBtn = document.querySelector(".nav-toggler"),
+  aside = document.querySelector(".aside");
+navTogglerBtn.addEventListener("click", () => {
+  asideSectionTogglerBtn();
+});
+
+function asideSectionTogglerBtn() {
+  aside.classList.toggle("open");
+  navTogglerBtn.classList.toggle("open");
+  for (let i = 0; i < totalSection; i++) {
+    allSection[i].classList.toggle("open");
+  }
 }
 // 네비게이션 버튼 클릭시 동작 및 스크롤 하단
 // const navBtn = document.querySelectorAll(".nav li a");
